286eff112f59c26b25b4ee308d0d34862d2aba09
Yanare Ku <lurial@gmail.com>
2018-08-31 18:27:55 -0700

    Fetching heroes from Marvel API and displaying them with React.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Heroes extends Component {
+  state = {
+    heroes: []
+  };
+
+  componentDidMount() {
+    const url = 'https://gateway.marvel.com:443/v1/public/characters?apikey=053864010f911ad7cb34d3f11377af19';
+    fetch(url)
+    .then(response => {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then((data) => {
+        console.log("data", data.data.results);
+        const heroes = data.data.results;
+        this.setState({heroes})
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="hero-container">
+        {this.state.heroes.map(hero => (
+          // Turn this into another react component
+          <div className="hero">
+            <img src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} />
+            <div>{hero.name}</div>
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-export default App;
+export default Heroes;
